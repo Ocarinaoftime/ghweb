7808f1c72c31debe7fae5654a78a05baaf83420c
ocarinaoftime <botwguardianbotw@gmail.com>
2023-09-11 13:49:08 -0400

    f

--- server.mjs
+++ server.mjs
@@ -1,13 +1,19 @@
 import { createServer } from "http";
 import { Server } from "socket.io";
 
+var chart;
+
 const httpServer = createServer();
 const io = new Server(httpServer, {
   cors: {
     origin: "*"
   },
 });
-io.on("connection", () => {
+io.on("connection", (socket) => {
     console.log("connected!!!")
+    socket.on("chart send", (arg) => {
+      arg = chart;
+      console.log(chart)
+    }) 
 })
 io.listen(3000)
