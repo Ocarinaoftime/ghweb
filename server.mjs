7a2c9774c3ef41ac39683b16b1e131c0dfbd5d3f
ocarinaoftime <botwguardianbotw@gmail.com>
2023-09-12 12:23:14 -0400

    f

--- server.mjs
+++ server.mjs
@@ -1,7 +1,13 @@
 import { createServer } from "http";
 import { Server } from "socket.io";
-import { writeFile } from "fs";
+import * as fs from "fs";
+import path from "path"
+import { fileURLToPath } from "url";
+import * as rl from "readline"
 var chart;
+const __filename = fileURLToPath(import.meta.url)
+
+const __dirname = path.dirname(__filename)
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
@@ -13,10 +19,17 @@ io.on("connection", (socket) => {
     console.log("connected!!!")
     socket.on("upload", (file, callback) => {
       console.log(file)
-
-      writeFile("./tmp/upload", file, (err) => {
+      var r = rl.createInterface({
+        input : fs.createReadStream(file)
+      })
+      r.on('line', function (text) {
+        console.log(text)
+      })
+      fs.writeFile(__dirname + "/tmp/upload/chart.txt", file, (err) => {
         callback({ message: err ? "failure" : "success" });
       });
+      
     });
 });
+rl.
 io.listen(3000)
